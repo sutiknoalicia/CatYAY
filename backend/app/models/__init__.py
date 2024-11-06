f9686ff71ef708d6de902ae255d9448b4fe564f5
gvalencios <georgyvalencio77@gmail.com>
2024-11-06 11:40:11 +0800

    delete models

--- backend/app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file can be used to initialize the database and import models
-from .database import Base, engine
-
-# Import all models here
-# from .user import User
-# from .booking import Booking
-
-# Create all tables
-Base.metadata.create_all(bind=engine)
