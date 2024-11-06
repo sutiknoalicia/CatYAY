b47b53832df81befa2d4bcb429978192ecf1d1ef
Howard <howard2001wilson@gmail.com>
2024-11-06 11:09:35 +0800

    Added useFonts hook and Header for main page

---
+++ helpers/FontFamilies.ts
@@ -0,0 +1,10 @@
+export const FontFamilies = {
+  SofiaProRegular: "SofiaProRegular",
+  SofiaProBlack: "SofiaProBlack",
+  SofiaProBold: "SofiaProBold",
+  SofiaProExtraLight: "SofiaProExtraLight",
+  SofiaProLight: "SofiaProLight",
+  SofiaProMedium: "SofiaProMedium",
+  SofiaProSemiBold: "SofiaProSemiBold",
+  SofiaProUltraLight: "SofiaProUltraLight",
+};
