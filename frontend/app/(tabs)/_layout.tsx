3e07d57813172e8d86f106139190eda060e84b22
Alicia Sutikno <99207714+sutiknoalicia@users.noreply.github.com>
2024-11-06 21:34:25 +0800

    Selesaiin journey (#16)

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -51,6 +51,18 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="journey"
+        options={{
+          title: "Journey",
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? "code-slash" : "code-slash-outline"}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
