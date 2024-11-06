2b1c00fb7ab85370b93a746a732d285049ad3f00
sutiknoalicia <alicias@connect.hku.hk>
2024-11-06 13:13:30 +0800

    resolve merge conflict

--- frontend/app/(tabs)/ticket.tsx
+++ frontend/app/(tabs)/ticket.tsx
@@ -1,4 +1,4 @@
-import { TicketCard } from "@/components/TicketCard";
+import { TicketCard } from "../../views/ticketing/TicketCard";
 import { View } from "react-native";
 
 export default function TicketScreen() {
@@ -18,38 +18,6 @@ export default function TicketScreen() {
     onViewDetails: () => console.log("Flight details viewed"),
   };
 
-  const trainTicket = {
-    transportType: "train" as "train",
-    identifier: "TR 203",
-    duration: "2h 30m",
-    departureTime: "09:00",
-    departureLocation: "LDN",
-    arrivalTime: "11:30",
-    arrivalLocation: "MNC",
-    carbonEmissions: 2,
-    class: "Standard",
-    currency: "IDR",
-    price: 75000.0,
-    onSelect: () => console.log("Train selected"),
-    onViewDetails: () => console.log("Train details viewed"),
-  };
-
-  const ferryTicket = {
-    transportType: "ferry" as "ferry",
-    identifier: "FR 108",
-    duration: "45m",
-    departureTime: "14:00",
-    departureLocation: "HKI",
-    arrivalTime: "14:45",
-    arrivalLocation: "MCU",
-    carbonEmissions: 5,
-    class: "Standard",
-    currency: "IDR",
-    price: 50000.0,
-    onSelect: () => console.log("Ferry selected"),
-    onViewDetails: () => console.log("Ferry details viewed"),
-  };
-
   return (
     <View
       style={{
@@ -61,8 +29,6 @@ export default function TicketScreen() {
       }}
     >
       <TicketCard {...flightTicket} />
-      <TicketCard {...trainTicket} />
-      <TicketCard {...ferryTicket} />
     </View>
   );
 }
